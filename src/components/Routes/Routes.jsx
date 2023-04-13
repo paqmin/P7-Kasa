b428103b92ee1a1bdf7ae7680a61c7433f80cf04
Frizon Mahalia <mahalia.frizon@gmail.com>
2023-04-13 12:29:23 +0200

    homepage

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 import Accueil from '../../pages/Accueil/Accueil'
 import Apropos from '../../pages/Apropos/Apropos'
 import Layout from '../../pages/Layout/Layout'
@@ -13,7 +14,7 @@ function RoutesPath() {
             <Layout>
                     <Routes basename={"/P7-Kasa"}>
                         <Route path="/accueil" element={<Accueil />} />
-                        <Route path="/" element={<Accueil />} />
+                        <Route path="/" element={<Navigate replace to="/accueil" />} />
                         <Route path="/logement/:id" element={<Fiche />}/>
                         <Route path="/a-propos" element={<Apropos />} />
                         <Route path="*" element={<Error />} />

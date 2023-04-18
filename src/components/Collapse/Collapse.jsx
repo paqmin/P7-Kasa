49f1821ca01e04e3560b14c60624e708c557cbd0
Frizon Mahalia <mahalia.frizon@gmail.com>
2023-04-18 16:09:51 +0200

    open -closed

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -5,17 +5,17 @@ import fleche from '../../assets/fleche.svg'
 
 
 function Collapse(data){
-    const [isOpen, setIsOpen] = useState(true)
-    return isOpen ?(
+    const [isClosed, setIsClosed] = useState(false)
+    return isClosed ?(
         <article className='kasa-collapse' >
-            <button className="titre-collapse" onClick={() => setIsOpen(true)}>
+            <button className="titre-collapse" onClick={() => setIsClosed(false)}>
                 {data.titre} <img className="fleche-up" src={fleche} alt="fleche" />
             </button>
             <div className="description-collapse"> {data.texte}</div>
         </article>
 ) : (
     <article className='kasa-collapse' >
-        <button className="titre-collapse" onClick={() => setIsOpen(false)}>
+        <button className="titre-collapse" onClick={() => setIsClosed(true)}>
             {data.titre} 
             <img className="fleche-down" src={fleche} alt="fleche" />
         </button> 
